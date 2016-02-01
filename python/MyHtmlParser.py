cee37fe8a91b3cda56f161618757f55a2bc422ee
Daniel Talbert <dantalbertsemail@gmail.com>
2016-01-31 21:34:48 -0700

    Fix ascii encoding error

--- python/MyHtmlParser.py
+++ python/MyHtmlParser.py
@@ -5,12 +5,8 @@ from HTMLParser import HTMLParser
 class MyHtmlParser(HTMLParser):
         def __init__(self):
             HTMLParser.__init__(self)
-            self.links = ["test string"]
-            # print self.links
+            self.links = []
 
         def handle_data(self, data):
             if data == "Head of Business Travel":
-                # print data
                 self.links.append(data)
-                # print self.links
-                print self.links.count("Head of Business Travel")
--- python/testImport.py
+++ python/testImport.py
@@ -2,21 +2,28 @@
 import urllib2
 from MyHtmlParser import MyHtmlParser
 
+translationMap = ''
+for i in range(256):
+    if i <= 127:
+        translationMap += chr(i)
+    else: # Replace high-end ascii characters with '?'
+        translationMap += '?'
+
 
 def HtmlRetriever(url):
     req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
     con = urllib2.urlopen(req)
-    return con.read()
+    return con.read().translate(translationMap)
 
 
 def main():
     url = "https://www.airbnb.com/careers/departments/business-development"
 
     html = HtmlRetriever(url)
+    print "length of html string:", len(html)
 
     parser = MyHtmlParser()
     parser.feed(html)
-    print parser.links
-
+    print "parser links 2: ", parser.links
 
 main()
